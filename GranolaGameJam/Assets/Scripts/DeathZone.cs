320c373f1a9e10941426833fc9de5a18097be2fd
milo <miloqureshi02@gmail.com>
2024-10-05 13:07:49 -0400

    coin collectible

---
+++ GranolaGameJam/Assets/Scripts/DeathZone.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathZone : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private bool inZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Check if the player is in the kill zone
+        if (collision.gameObject.CompareTag("DeathZone"))
+        {
+            inZone = true;
+            // go to next scene using scene manager
+        }
+    }
+
+
+}
--- GranolaGameJam/Assets/Scripts/PlayerController.cs
+++ GranolaGameJam/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5f;
     private bool isGrounded;
     private Rigidbody2D rb;
+    private Inventory inventory;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        inventory = GetComponent<Inventory>();
     }
 
     void Update()
@@ -44,4 +46,18 @@ public class PlayerController : MonoBehaviour
             isGrounded = false;
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Coin")
+        {
+            Debug.Log(collision.gameObject.name + " is the player");
+
+            inventory.keyCount++;
+
+            //destroys attached game object
+            Destroy(collision.gameObject);
+        }
+    }
+   
 }
